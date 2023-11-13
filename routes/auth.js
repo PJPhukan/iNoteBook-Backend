6197eedfcd5be3b87dafd549b82e49aad8ae529a
PJPhukan <paragjyotiphukan2004@gmail.com>
2023-11-14 00:13:11 +0530

    Completed  login endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -78,11 +78,67 @@ router.post('/createuser', checkSchema({
    } catch (error) {
       //catch error
       console.error(error.massage);
-      res.status(500).json("Some error occured!");
+      res.status(400).send("Internal Server Error");
    }
 
 
 })
 
+
+
+
+
+//EndPoints:::---->>>Authonticate a user Using:-> POST '/api/auth/login' ,no login required
+router.post('/login', checkSchema({
+   //email schema check
+   email: { isEmail: true, errorMessage: "Enter a valid email!" },
+   password: { errorMessage: "Password can't be blank" }
+}), async (req, res) => {
+
+   //if there are error,return an error with an bad request 400
+   const errors = validationResult(req);
+   if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+   }
+
+   //destructure email and password
+   const { email, password } = req.body;
+
+   try {
+      //Check user email exists or not in the database
+      const user = await User.findOne({ email })
+      // console.log(user)
+      //If user email does not exists in the database then it return an error with an bad request 400.
+      if (!user) {
+         return res.status(400).json({ error: "Please enter correct credentials" })
+      }
+
+      //compare user passoed with the database for login
+      const passwordCompare = await bcrypt.compare(password, user.password);//It returns a boolean value 
+      if (!passwordCompare) {
+         //if user detail does not match with the user database information then return a bed requast and an error massage 
+         return res.status(400).json({ error: "Please enter correct credentials" })
+      }
+
+
+      //If above all the details correct then return the user id 
+      const data = {
+         user: {
+            id: user.id
+         }
+      }
+      const authToken = jwt.sign(data, JWD_SECREAT)
+
+      //Return the user authotication token
+      res.json({ authToken })
+
+   } catch (error) {
+      //If error occured then handling the error in the catch block
+      console.error(error.massage);
+      // console.log(error)
+      res.status(500).send("Internal Server Error");
+   }
+
+})
 
 module.exports = router;
